5377bd29ed2935290644d2d521b8af55f70f7dbc
Pritom Deb <pritom.deb@walmartlabs.com>
2020-03-14 08:10:23 +0530

    Adding numbers LinkedList : Cleaner way without duplicate code

--- src/practice/linkedList/AddingNumbers.java
+++ src/practice/linkedList/AddingNumbers.java
@@ -2,39 +2,42 @@ package practice.linkedList;
 
 public class AddingNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int carry = 0;
-        ListNode result = new ListNode(-1);
-        ListNode head = result;
-        while (l1 != null && l2 != null) {
-            int temp = l1.val + l2.val + carry;
-            carry = temp / 10;
-            temp = temp % 10;
-            result.next = new ListNode(temp);
-            result = result.next;
-            l1 = l1.next;
-            l2 = l2.next;
-        }
+        ListNode head;
+        ListNode ls = new ListNode(0);
+        head=ls;
+        int carry=0,a=0,sum=0;
+        while((l1!=null || l2!=null) || carry==1)
+        {
+            sum=0;
+            if(l1!=null)
+            {
+                sum = sum + l1.val;
+                l1 = l1.next;
+            }
+            if(l2!=null)
+            {
+                sum = sum + l2.val;
+                l2 = l2.next;
+            }
+            if(carry==1)
+            {
+                sum = sum + carry;
+                carry=0;
+            }
+            if(sum>9)
+            {
+                a = sum%10;
+                carry = 1;
+                ls.next = new ListNode(a);
+                ls= ls.next;
+            }
+            else
+            {
+                ls.next = new ListNode(sum);
+                ls= ls.next;
+            }
 
-        while (l1 != null) {
-            int temp = l1.val + carry;
-            carry = temp / 10;
-            temp = temp % 10;
-            result.next = new ListNode(temp);
-            result = result.next;
-            l1 = l1.next;
         }
-
-        while (l2 != null) {
-            int temp = l2.val + carry;
-            carry = temp / 10;
-            temp = temp % 10;
-            result.next = new ListNode(temp);
-            result = result.next;
-            l2 = l2.next;
-        }
-
-        if (carry != 0)
-            result.next = new ListNode(carry);
         return head.next;
     }
 
