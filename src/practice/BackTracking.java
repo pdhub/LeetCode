d39c4eeca140e21e5e021cf4301077c0a8e2371c
Pritom Deb <pritom.deb@walmartlabs.com>
2020-03-18 08:36:14 +0530

    Letter Combinations of a Phone Number(Wrong submit)

---
+++ src/practice/BackTracking.java
@@ -0,0 +1,40 @@
+package practice;
+
+import java.util.*;
+
+public class BackTracking {
+
+
+    static Map<Integer, String> digitToCharaters = new HashMap<Integer, String>();
+
+    {
+        digitToCharaters.put(1, "");
+        digitToCharaters.put(2, "abc");
+        digitToCharaters.put(3, "def");
+        digitToCharaters.put(4, "ghi");
+        digitToCharaters.put(5, "jkl");
+        digitToCharaters.put(6, "mno");
+        digitToCharaters.put(7, "pqrs");
+        digitToCharaters.put(8, "tuv");
+        digitToCharaters.put(9, "wxyz");
+    }
+
+    public List<String> letterCombinations(String inputStr) {
+        if (inputStr.length() == 0){
+            return Arrays.asList("");
+        }
+
+        char ch = inputStr.charAt(0);
+        String restOfString = inputStr.substring(1);
+
+        List<String> prev = letterCombinations(restOfString);
+        List<String> res = new ArrayList<String>();
+        String digit = digitToCharaters.get((Integer.parseInt(ch+"")));
+        for (String l : prev){
+            for (int i = 0; i < digit.length(); i++) {
+                res.add(digit.charAt(i) + l);
+            }
+        }
+        return res;
+    }
+}
